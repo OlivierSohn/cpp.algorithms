801f7efdef5cf5447cc4cb9e8452b88423f9bd4f
Olivier Sohn <olivier.sohn@gmail.com>
2017-01-29 23:49:15 -0500

    control fade size

--- include/math.hpp
+++ include/math.hpp
@@ -85,5 +85,16 @@ namespace imajuscule {
     inline bool same_sign_strict(float a, float b) {
         return (a > 0.f && b > 0.f) || (a < 0.f && b < 0.f);
     }
+    
+    constexpr float clamp(float v, float v1, float v2) {
+        if(v < v1) {
+            return v1;
+        }
+        if(v> v2) {
+            return v2;
+        }
+        return v;
+    }
+    
 } // NS imajuscule
 

dff810857f1342738f7d4a0489a95474975adf64
Olivier Sohn <olivier.sohn@gmail.com>
2017-04-18 17:34:34 +0200

    new tests

--- test/test_dsp.convolution.cpp
+++ test/test_dsp.convolution.cpp
@@ -2,53 +2,114 @@
 namespace imajuscule {
     namespace testdspconv {
         
-        template<typename Convolution>
-        void testDirac() {
+        template<typename T>
+        auto makeNonTrivialCoefficients() {
+            return std::vector<T>{{ .9,.8,.7,.6,.3,.2,.1,0. }};
+        }
+        
+        template<typename T>
+        auto makeTrivialCoefficients() {
+            return std::vector<T>{{ 1. }};
+        }
+        
+        template<typename T>
+        auto makeTrivialCoefficients2() {
+            return std::vector<T>{{ -1. }}; // makes partitionned test fail
+        }
+        
+        template<typename T>
+        auto makeCoefficients(int coeffs_index) {
+            switch(coeffs_index) {
+                case 0: return makeTrivialCoefficients<T>();
+                case 1: return makeTrivialCoefficients2<T>();
+                default:
+                    return makeNonTrivialCoefficients<T>();
+            }
+        }
+        
+        template<typename Convolution, typename Coeffs>
+        bool test(Convolution & conv, Coeffs const & coefficients) {
+            using T = typename Convolution::FPT;
             using namespace fft;
+            
+            conv.setCoefficients(coefficients);
+            
+            // feed a dirac
+            conv.step(1);
+            for(int i=1; i<conv.getComputationPeriodicity(); ++i) {
+                conv.step(0);
+            }
+            
+            std::vector<T> results;
+            for(int i=0; i<coefficients.size(); ++i) {
+                results.push_back(conv.get());
+                conv.step(0);
+            }
+
+            // we sum the results of each partition so the epsilon is (worst case) :
+            auto eps = conv.countPartitions() * getFFTEpsilon<T>(conv.get_fft_length());
+            EXPECT_EQ(coefficients.size(), results.size());
+            for(auto j=0; j<results.size(); ++j) {
+                EXPECT_NEAR(coefficients[j], results[j], eps);
+            }
+            return false;
+        }
+        
+        template<typename Convolution>
+        bool testDiracPartitionned(int coeffs_index) {
             
             using T = typename Convolution::FPT;
             
-            const std::vector<T> coefficients {{ .9,.8,.7,.6,.3,.2,.1 }};
-
-            for(int i=0; i<5;i++){
+            const auto coefficients = makeCoefficients<T>(coeffs_index);
+            
+            int i=0;
+            for(int i=0; i<5;i++)
+            {
                 auto const part_size = pow2(i);
                 Convolution conv;
                 
                 conv.set_partition_size(part_size);
-                conv.setCoefficients(coefficients);
-                
-                // feed a dirac
-                conv.step(1);
-                for(int i=1; i<conv.getComputationPeriodicity(); ++i) {
-                    conv.step(0);
-                }
-                std::vector<T> results;
-                for(int i=0; i<coefficients.size(); ++i) {
-                    results.push_back(conv.get());
-                    conv.step(0);
-                }
-                
-                // we sum the results of each partition so the epsilon is (worst case) :
-                auto eps = conv.countPartitions() * getFFTEpsilon<T>(conv.get_fft_length());
-                ASSERT_EQ(coefficients.size(), results.size());
-                for(auto j=0; j<results.size(); ++j) {
-                    ASSERT_NEAR(coefficients[j], results[j], eps) << j;
-                }
+                test(conv, coefficients);
             }
+            return false;
         }
-
-        void testDirac() {
-            //testDirac<FFTConvolution<float>>();
-            //testDirac<FFTConvolution<double>>();
-            testDirac<PartitionnedFFTConvolution<float>>();
-            testDirac<PartitionnedFFTConvolution<double>>();
+        
+        template<typename Convolution>
+        bool testDirac2(int coeffs_index) {
+            
+            using T = typename Convolution::FPT;
+            
+            const auto coefficients = makeCoefficients<T>(coeffs_index);
+            Convolution conv;
+            
+            test(conv, coefficients);
+            return false;
         }
-}
+        
+        template<typename Tag>
+        bool testDirac() {
+            using namespace fft;
+            for(int i=0; i<3; ++i) {
+                testDirac2<FFTConvolution<float, Tag>>(i);
+                testDirac2<FFTConvolution<double, Tag>>(i);
+                testDiracPartitionned<PartitionnedFFTConvolution<float, Tag>>(i);
+                testDiracPartitionned<PartitionnedFFTConvolution<double, Tag>>(i);
+            }
+            return false;
+        }
+    }
 }
 
 TEST(Convolution, dirac) {
     using namespace imajuscule;
     using namespace imajuscule::testdspconv;
     
-    testDirac();
+    testDirac<imj::Tag>();
+    
+#if __APPLE__
+    
+    testDirac<accelerate::Tag>();
+    
+#endif // __APPLE__
 }
+

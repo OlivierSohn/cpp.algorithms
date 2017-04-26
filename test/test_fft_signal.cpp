36a580ad418516988ddc16d5bc060d77734e43be
Olivier Sohn <olivier.sohn@gmail.com>
2017-04-26 15:47:20 +0200

    ++test

--- test/test_fft_signal.cpp
+++ test/test_fft_signal.cpp
@@ -78,6 +78,40 @@ namespace imajuscule {
             testCopy<Tag, float>();
             testCopy<Tag, double>();
         }
+        
+        template<typename Tag, typename T>
+        void testZero() {
+            using Sig = fft::RealSignal_<Tag, T>;
+            using namespace fft::slow_debug;
+ 
+            for(int N=0; N<10000; ++N){
+                a_64::vector<T> v;
+                v.resize(N, 1);
+                auto sig = Sig::make(std::move(v));
+
+                {
+                    auto const res = unwrap_signal<Tag>(sig, N);
+                    for(int i=0; i<N; ++i){
+                        ASSERT_EQ(1, res[i].real());
+                    }
+                }
+                
+                Sig::zero(sig);
+                
+                {
+                    auto const res = unwrap_signal<Tag>(sig, N);
+                    for(int i=0; i<N; ++i){
+                        ASSERT_EQ(0, res[i].real());
+                    }
+                }
+            }
+        }
+        
+        template<typename Tag>
+        void testZero() {
+            testZero<Tag, float>();
+            testZero<Tag, double>();
+        }
     }
 }
 
@@ -106,5 +140,14 @@ TEST(FFTSignal, copy) {
     
     for_each(fft::Tags, [](auto t) {
         testCopy<decltype(t)>();
+    });
+}
+
+TEST(FFTSignal, zero) {
+    using namespace imajuscule;
+    using namespace imajuscule::testfftsignal;
+    
+    for_each(fft::Tags, [](auto t) {
+        testZero<decltype(t)>();
     });
 }

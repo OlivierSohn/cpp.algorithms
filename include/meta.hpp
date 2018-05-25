1f9997a4769a1a7825ca971cec6af2f82912cc58
Olivier Sohn <olivier.sohn@gmail.com>
2018-05-26 00:20:14 +0200

    Minor change

--- include/meta.hpp
+++ include/meta.hpp
@@ -10,18 +10,18 @@ void operator=(T const &t) = delete; \
 T(T &&) = delete;
 
 namespace imajuscule {
-    
+
     template<class F, class TUPLE, std::size_t...Is>
     void for_each(TUPLE & tuple, F func, std::index_sequence<Is...>){
         using expander = int[];
         (void)expander { 0, ((void)func(std::get<Is>(tuple)), 0)... };
     }
-    
+
     template<class F, class TUPLE>
     void for_each(TUPLE & tuple, F func){
         for_each(tuple, func, std::make_index_sequence<std::tuple_size<TUPLE>::value>());
     }
-    
+
     template<int N, typename... Ts>
     using NthTypeOf = typename std::tuple_element<N, std::tuple<Ts...>>::type;
 
@@ -32,19 +32,19 @@ namespace imajuscule {
     private:
         class Yes { char a[1]; };
         class No { char a[10]; };
-        
+
         static Yes Test( B* ); // undefined
         static No Test( ... ); // undefined
-        
+
     public:
-        static constexpr bool Is = sizeof(Test(safe_cast<D*>(nullptr))) == sizeof(Yes) ? 1 : 0;
+        static constexpr bool value = sizeof(Test(safe_cast<D*>(nullptr))) == sizeof(Yes) ? 1 : 0;
     };
-    
+
     template<typename F>
     struct Periodically {
-        
+
         NON_COPYABLE_NOR_MOVABLE(Periodically);
-        
+
         Periodically(int period, F f) : f(f), period(period) {}
         ~Periodically() {
             if(i) {
@@ -52,7 +52,7 @@ namespace imajuscule {
                 f();
             }
         }
-        
+
         void step() {
             ++i;
             if(i >= period) {
@@ -65,7 +65,6 @@ namespace imajuscule {
         int period;
         F f;
     };
-    
+
 
 } // NS imajuscule
-

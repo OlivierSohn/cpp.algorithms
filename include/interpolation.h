2b50d87558b102f528b4b4841f16f146dfa21f09
Olivier Sohn <olivier.sohn@gmail.com>
2018-06-02 16:01:36 +0200

    Add methodç

--- include/interpolation.h
+++ include/interpolation.h
@@ -45,12 +45,12 @@ namespace imajuscule
 
             INTERPOLATION_UPPER_BOUND,
         };
-        
+
         static const float NO_EASE;
         static const float EASE_IN;
         static const float EASE_OUT;
         static const float EASE_IO;
-        
+
         // Naming convention: polynomial is "P{degree}"
 
         // values for NO_EASE:
@@ -65,8 +65,8 @@ namespace imajuscule
         static const float SINE;
         static const float EXPO;
         static const float CIRC;
-        
-        
+
+
         static constexpr bool isValid( interpolation type )
         {
             switch(type)
@@ -122,7 +122,16 @@ namespace imajuscule
             }
             return false;
         }
-        
+
+        static interpolation toItp(int i) {
+          auto v = static_cast<interpolation>(i);
+          if (isValid(v)) {
+            return v;
+          }
+          Assert(0 && "interpolation is not valid");
+          return itp::LINEAR;
+        }
+
         static const enumTraversal & interpolation_traversal();
 
         // returns true if value is an interpolation
@@ -131,13 +140,13 @@ namespace imajuscule
         static const char * interpolationInfo(int);
 
         // cf. http://gizma.com/easing/
-        
+
         // parameters are :
         // t : time (from 0 to d)
         // d : duration
         // b : value at t==0
         // c : added value i.e b+c is the value at t==d
-        
+
         // wraps the other functions
         static float interpolate(interpolation type, float t_start, float t_cur, float t_end, float val_start, float val_end) {
             return interpolate(type
@@ -146,130 +155,130 @@ namespace imajuscule
                                , val_end - val_start
                                , t_end - t_start);
         }
-        
+
         static float interpolate(interpolation type, float t_start_to_cur, float val_start = 0.f, float val_range = 1.f, float t_start_to_end = 1.f);
-       
+
         static float linearTween(float t, float b, float c, float d);
         static float proportional_value_derivative(float t, float b, float c, float d);
 
         // quadratic easing in - accelerating from zero velocity
-        
+
         static float easeInQuad(float t, float b, float c, float d);
-        
+
         // quadratic easing out - decelerating to zero velocity
-        
+
         static float easeOutQuad(float t, float b, float c, float d);
-        
+
         // quadratic easing in/out - acceleration until halfway, then deceleration
-        
+
         static float easeInOutQuad(float t, float b, float c, float d);
-        
+
         // cubic easing in - accelerating from zero velocity
-        
+
         static float easeInCubic(float t, float b, float c, float d);
-        
+
         // cubic easing out - decelerating to zero velocity
-        
+
         static float easeOutCubic(float t, float b, float c, float d);
-        
+
         // cubic easing in/out - acceleration until halfway, then deceleration
-        
+
         static float easeInOutCubic(float t, float b, float c, float d);
-        
+
         // quartic easing in - accelerating from zero velocity
-        
+
         static float easeInQuart(float t, float b, float c, float d);
-        
+
         // quartic easing out - decelerating to zero velocity
-        
+
         static float easeOutQuart(float t, float b, float c, float d);
-        
+
         // quartic easing in/out - acceleration until halfway, then deceleration
-        
+
         static float easeInOutQuart(float t, float b, float c, float d);
-        
+
         // quintic easing in - accelerating from zero velocity
-        
+
         static float easeInQuint(float t, float b, float c, float d);
-        
+
         // quintic easing out - decelerating to zero velocity
-        
+
         static float easeOutQuint(float t, float b, float c, float d);
-        
+
         // quintic easing in/out - acceleration until halfway, then deceleration
-        
+
         static float easeInOutQuint(float t, float b, float c, float d);
-        
+
         // sinusoidal easing in - accelerating from zero velocity
-        
+
         static float easeInSine(float t, float b, float c, float d);
-        
+
         // sinusoidal easing out - decelerating to zero velocity
-        
+
         static float easeOutSine(float t, float b, float c, float d);
-        
+
         // sinusoidal easing in/out - accelerating until halfway, then decelerating
-        
+
         static float easeInOutSine(float t, float b, float c, float d);
-        
+
         // exponential easing in - accelerating from zero velocity
-        
+
         static float easeInExpo(float t, float b, float c, float d);
-        
+
         // exponential easing out - decelerating to zero velocity
-        
+
         static float easeOutExpo(float t, float b, float c, float d);
-        
+
         // exponential easing in/out - accelerating until halfway, then decelerating
-        
+
         static float easeInOutExpo(float t, float b, float c, float d);
-        
+
         // circular easing in - accelerating from zero velocity
-        
+
         static float easeInCirc(float t, float b, float c, float d);
-        
+
         // circular easing out - decelerating to zero velocit
-        
+
         static float easeOutCirc(float t, float b, float c, float d);
-        
+
         // circular easing in/out - acceleration until halfway, then deceleration
-        
+
         static float easeInOutCirc(float t, float b, float c, float d);
 
         private:
             static enumTraversal gInterpolationTraversal;
     };
-    
+
     template<typename T = float>
     struct NormalizedInterpolation {
         using Tr = NumTraits<T>;
         static constexpr T zero = Tr::zero();
-        
+
         NormalizedInterpolation() = default;
-        
+
         NormalizedInterpolation(itp::interpolation i) {
             setInterpolation(i);
         }
-        
+
         void setInterpolation(itp::interpolation i) {
             interp = i;
             assert(itp::intIsReal(interp));
         }
-        
+
         T get_value(T t_cur, T t_end, T val_start, T val_end) const {
             assert(t_cur >= zero); // else need to return val_start
             assert(itp::intIsReal(interp));
-            
+
             if(t_cur > t_end) {
                 return val_end;
             }
-            
+
             return get_unfiltered_value(t_cur,
                                         t_end,
                                         val_start,
                                         val_end);
         }
-        
+
         T get_unfiltered_value(T t_cur, T t_end, T val_start, T val_end) const {
             return itp::interpolate(interp,
                                     zero /* t start */,
@@ -278,7 +287,7 @@ namespace imajuscule
                                     val_start,
                                     val_end);
         }
-        
+
     private:
         itp::interpolation interp : 5;
     };

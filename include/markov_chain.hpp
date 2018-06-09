6d8d2fb0707a6507338c34ff59d103f4ad4fced0
Olivier Sohn <olivier.sohn@gmail.com>
2018-06-09 18:01:55 +0200

    Format

--- include/markov_chain.hpp
+++ include/markov_chain.hpp
@@ -1,12 +1,6 @@
-/* Copyright (C) Olivier Sohn - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Olivier Sohn <olivier.sohn@gmail.com>, 2017
- */
-
 namespace imajuscule {
-    
-    enum class Move {
+
+  enum class Move {
         CREATE,
         DELETE,
         ENTER,

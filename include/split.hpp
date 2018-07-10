127e4ce29651e07be83047a29b81b332b2d69198
Olivier Sohn <olivier.sohn@gmail.com>
2018-07-10 22:02:59 +0200

    Add a constructor

--- include/split.hpp
+++ include/split.hpp
@@ -10,22 +10,29 @@ namespace imajuscule {
    */
   template<typename U, U nHigh, U nLow>
   struct Split {
-    
+
     static_assert(std::is_unsigned_v<U>);
-    
+
     static_assert(nLow + nHigh == 8*sizeof(U));
-    
+
     static_assert(nLow > 0);
     static_assert(nHigh > 0);
-    
+
     static constexpr U maxHigh = std::numeric_limits<U>::max() >> nLow;
     static constexpr U maxLow  = std::numeric_limits<U>::max() >> nHigh;
-    
+
     static constexpr U highMask = ~maxLow;
     static_assert(highMask == std::numeric_limits<U>::max() - maxLow);
-    
+
     Split() = default;
     Split(U raw) : raw(raw) {}
+
+    // for safety, we mask 'low' so that it won't modify the bits where
+    // 'high' is stored, if it's out of bounds.
+    Split(U high, U low) : raw((high << nLow) + (low & maxLow) {
+      Assert(low <= maxLow); // else, some information is lost
+      Assert(high <= maxHigh); // else, some information is lost
+    }
 
     U getHighWithZeros() const {
       return raw & highMask;
